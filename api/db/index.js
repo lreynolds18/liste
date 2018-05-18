672a039b46b6d2a800685db285e4d48d1ffeb26c
Lucas Reynolds <lreynolds18@gmail.com>
2018-05-18 19:27:36 -0400

    changed mongoDB to postgresql.  Added reciept and grocery picture schemas -- ingredient, shopping list, recipe are being fazed out for now

--- api/db/index.js
+++ api/db/index.js
@@ -1,6 +1,9 @@
+var database_info = require('./database_info');
+
 const { Pool } = require('pg')
 
-const pool = new Pool();
+
+const pool = new Pool(database_info.db_info);
 
 module.exports = {
   query: (text, params, callback) => {
